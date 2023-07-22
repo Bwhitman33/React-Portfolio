937cebff02f702aa18d6cb7f5872159ca51a1be8
Bwhitman33 <bwhitman33@gmail.com>
2023-07-22 11:40:47 -0400

    Added header component code, errors abound. Testing now.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function header() {
+    return (
+    <div className="header">
+        <h1>Brandon Whitman</h1>
+        <ul className='navbar'>
+          <li>About Me</li>
+          <li>Work Showcase</li>
+          <li>Contact Me</li>
+          <li>Resume</li>
+        </ul>
+    </div>
+    );
+}
