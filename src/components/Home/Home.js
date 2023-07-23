fa7716ef7d954d59613cd408a411991f7d2cb108
Bwhitman33 <bwhitman33@gmail.com>
2023-07-23 12:36:38 -0400

    Home page finished and styled

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Selfie from "../../assets/pictures/selfie.jpg";
 import './Home.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faHtml5, 
+    faCss3,
+    faGit,
+    faJs,
+    faBootstrap,
+    faNodeJs,
+    faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
     return (
@@ -8,40 +19,34 @@ export default function Home() {
             <div className="img-cont">
                 <img src={Selfie} alt="Me!" className="selfie" />
             </div>
-            <div>
+            <div className="tech-list">
                 <h2>
                     Proficient Technologies
                 </h2>
                 <ul className= "Technologies">
                     <li>
-                        HTML
+                        <FontAwesomeIcon icon={faHtml5} color="#e34f26" size="4x" />
                     </li>
                     <li>
-                        CSS
+                        <FontAwesomeIcon icon={faCss3} color="#1680C0" size="4x" />
                     </li>
                     <li>
-                        Git
+                        <FontAwesomeIcon icon={faGit} color="#333" size="4x" />
                     </li>
                     <li>
-                        Javascript
+                        <FontAwesomeIcon icon={faJs} color="#F0DB4F" size="4x" />
                     </li>
                     <li>
-                        Bootstrap
+                        <FontAwesomeIcon icon={faBootstrap} color="#514999" size="4x" />
                     </li>
                     <li>
-                        Node.js
+                        <FontAwesomeIcon icon={faNodeJs} color="#529d42" size="4x" />
                     </li>
                     <li>
-                        Express.js
+                        <FontAwesomeIcon icon={faDatabase} color="#5d84f2" size="4x" />
                     </li>
                     <li>
-                        SQL
-                    </li>
-                    <li>
-                        Mongo
-                    </li>
-                    <li>
-                        React
+                        <FontAwesomeIcon icon={faReact} color="#00d7fe" size="4x" />
                     </li>
                 </ul>
             </div>
