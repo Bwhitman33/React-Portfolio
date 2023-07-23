8bdde4aaf0e7b97e5c3d518b9a4655ffc2b71f43
Bwhitman33 <bwhitman33@gmail.com>
2023-07-23 11:54:27 -0400

    Styling changes and additions. Home page coming along

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Bio from './components/About/About';
+import Home from './components/Home/Home';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
@@ -11,8 +12,8 @@ export default function App() {
       <Header />
       <div className='body'>
         <Routes>
+          <Route path="*" element={<Home />} />
           <Route path="/about-me" element={<Bio />} />
-          {/* <Route path="/" element={<Bio />} /> */}
         </Routes>
       </div>
       <Footer />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,50 @@
-// import React from "react";
+import React from "react";
+import Selfie from "../../assets/pictures/selfie.jpg";
+import './Home.css'
 
-// export default function Home() {
-//     return (
-//         <main>
-//             {/* Body of the Portfolio is here */}
-//         </main>
-//     )
-// }
+export default function Home() {
+    return (
+        <section className="Home">
+            <div className="img-cont">
+                <img src={Selfie} alt="Me!" className="selfie" />
+            </div>
+            <div>
+                <h2>
+                    Proficient Technologies
+                </h2>
+                <ul className= "Technologies">
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        Git
+                    </li>
+                    <li>
+                        Javascript
+                    </li>
+                    <li>
+                        Bootstrap
+                    </li>
+                    <li>
+                        Node.js
+                    </li>
+                    <li>
+                        Express.js
+                    </li>
+                    <li>
+                        SQL
+                    </li>
+                    <li>
+                        Mongo
+                    </li>
+                    <li>
+                        React
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
+}
